2214aab5d36495bde999ee2e5473cfff01eb0437
fantasyfriend <erwinvanhouten@yahoo.com>
2017-10-23 17:05:40 +0200

    oef1 aangepast

--- javascript/basisstructuren/oef1.js
+++ javascript/basisstructuren/oef1.js
@@ -4,10 +4,10 @@ var toetsenbord = require('readline-sync');
 
 var eerste = toetsenbord.question('eerste getal:  ');
 var tweede = toetsenbord.question('tweede getal:  ');
-var som = parseInt(eerste) + parseInt(tweede)
+var som = parseInt(eerste) * parseInt(tweede)
 console.log('Het product van %s en %s is %s', eerste, tweede, som);
-var loon = toetsenbord.question('loon (geheel getal):  ');
-var verhoging = toetsenbord.question('verhoging (geheel getal):  ');
-loon=parseInt(loon, 10);verhoging=parseInt(verhoging, 10)
+var loon = toetsenbord.question('loon :  ');
+var verhoging = toetsenbord.question('verhoging :  ');
+loon=parseFloat(loon);verhoging=parseFloat(verhoging)
 var nieuwLoon = loon + verhoging;
-console.log('Het nieuwe loon is %s,- = oude loon (%s,-) + verhoging (%s,-)', nieuwLoon, loon, verhoging);
+console.log('Het nieuwe loon is %d = oude loon (%d) + verhoging (%d)', nieuwLoon, loon, verhoging);
