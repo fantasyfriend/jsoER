b3118f6ed5b190af3cff326f85717a78098bf9ee
fantasyfriend <erwinvanhouten@yahoo.com>
2017-11-03 14:12:25 +0100

    updates

--- javascript/basisstructuren/oefstr9.js
+++ javascript/basisstructuren/oefstr9.js
@@ -6,12 +6,12 @@ var nummer;
 //functies
 function geslacht(nummer){
     var geslacht;
-    nummer.split(".")[1]%2==0?geslacht="vrouw":geslacht="man";
+    nummer.split("-")[1]%2==0?geslacht="vrouw":geslacht="man";
     console.log("U bent een "+geslacht);
 }
 function leeftijd(nummer){
     var leeftijd="";
-    leeftijd=(new Date().getFullYear())-(1900+parseInt(nummer.split(".")[0].slice(0,2)));
+    leeftijd=(new Date().getFullYear())-(1900+parseInt(nummer.split("-")[0].slice(0,2)));
     leeftijd>99?leeftijd-=100:leeftijd-=0;
     console.log("Uw leeftijd is "+leeftijd);
 }
