910e54b12b7030b91141c654f685bf999321cf25
fantasyfriend <erwinvanhouten@yahoo.com>
2017-11-30 13:20:29 +0100

    changes

--- javascript/basisstructuren/baking_start.js
+++ javascript/basisstructuren/baking_start.js
@@ -2,19 +2,17 @@ var cookies = {
 	instructions: "Preheat oven to 350...",
 	bake: function (time){
 		console.log("Baking the cookies.");
-        setTimeout(done, time);
+        setTimeout(function (){
+			console.log("Cookies are ready, take them out to cool.");
+			console.log("Cooling the cookies.");
+			setTimeout(function () {
+				console.log("Cookies are cool, time to eat!");
+			}, 1000);
+		}, time);
 	} 
 };
 
-function done(){
-	console.log("Cookies are ready, take them out to cool.");
-	console.log("Cooling the cookies.");
-	setTimeout(cooled, 1000);
-}
 
-function cooled() {
-	console.log("Cookies are cool, time to eat!");
-}
 
 console.log("Time to bake the cookies.");
 cookies.bake(2500);
--- javascript/basisstructuren/sodas00.js
+++ javascript/basisstructuren/sodas00.js
@@ -7,3 +7,22 @@ var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 }
                  { name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
                  { name: "Water", calories: 0, color: "clear", sold: 62123 }
                ];
+
+function compaire1(a,b){
+  if(a.sold>b.sold){return 1;}
+  else if (a.sold===b.sold){return 0;}
+  else {return -1;}
+}
+function compaire2(a,b){
+  if(a.name.toUpperCase()>b.name.toUpperCase()){return 1;}
+  else if (a.name.toUpperCase()===b.name.toUpperCase()){return 0;}
+  else {return -1;}
+}
+
+function sorteer(array,property){
+array[0][property].substring?array.sort(compaire2):array.sort(compaire1);
+}
+sorteer(products,"sold")
+
+console.log(products);
+products.map((v,i,a)=>console.log(a));
