da84b57410160308ee5190f1967b553ac0891fa9
fantasyfriend <erwinvanhouten@yahoo.com>
2017-12-05 13:29:04 +0100

    changes

--- javascript/browser/DOMStorage/tweetshirt/tweetshirt1.js
+++ javascript/browser/DOMStorage/tweetshirt/tweetshirt1.js
@@ -4,15 +4,47 @@ window.onload = function() {
 	var button = document.getElementById("previewButton");
 	button.onclick = previewHandler;
 	document.getElementById("shape").onchange=giveItemColor;
+	saveButton.onclick=save;
+	reloadButton.onclick=load;
+	
 
 	// Easter Egg ;-)
 	makeImage();
+}
+var sx=[],sy=[],sw=[],bgc,sh,ic,fgc,fs,tw,dr;
+
+function save(){
+	localStorage.bgc=bgc;
+	localStorage.shape=sh;
+	localStorage.ic=ic;
+	localStorage.fgc=fgc;
+	localStorage.fz=fs;
+	localStorage.tweets=tw;
+	localStorage.dr=dr;
+	localStorage.x=JSON.stringify(sx);
+	localStorage.y=JSON.stringify(sy);
+	localStorage.w=JSON.stringify(sw);
+}
+function load(){
+	if(localStorage.bgc){
+	backgroundColor.value=localStorage.bgc;
+	shape.value=localStorage.shape;
+	if(shape.value!='none')giveItemColor();
+	itemColor.value=localStorage.ic;
+	foregroundColor.value=localStorage.fgc;
+	fontSize.value=localStorage.fz;
+	tweets.value=localStorage.tweets;
+	draai.value=localStorage.dr;
+	sx=JSON.parse(localStorage.x);
+	sy=JSON.parse(localStorage.y);
+	sw=JSON.parse(localStorage.w);
+	previewHandler(1);}
 }
 function giveItemColor(){
 	itemColor.style.visibility="visible";
 }
 
-function previewHandler() {
+function previewHandler(l) {
 	var canvas = document.getElementById("tshirtCanvas");
 	var context = canvas.getContext("2d");
 	// there's no 3D canvas yet; this is to make code futureproof
@@ -21,18 +53,15 @@ function previewHandler() {
 
 	var selectObj = document.getElementById("shape");
 	var index = selectObj.selectedIndex;
-	var shape = selectObj[index].value;
+	var shape = sh = selectObj[index].value;
 
-	if (shape == "squares") {
-		for (var squares = 0; squares < 20; squares++) {
-			drawSquare(canvas, context);
-		}
-	}
-	else if (shape == "circles") {
-		for (var circles = 0; circles < 20; circles++) {
-			drawCircle(canvas, context);
-		}
-	}
+	if(l!=1){var l=0;sx=[];sy=[];sw=[];
+		for (var i = 0; i < 20; i++) {
+		var w = Math.floor(Math.random() * 40);sw.push(w);    
+		var x = Math.floor(Math.random() * canvas.width);sx.push(x);
+		var y = Math.floor(Math.random() * canvas.height);sy.push(y);
+	}}
+	drawShape(canvas,context,shape);
 	drawText(canvas, context);
 	drawBird(canvas, context);
 }
@@ -40,47 +69,38 @@ function previewHandler() {
 // This is where we'll set the background color
 function fillBackgroundColor(canvas, context) {
 	var selectObj = document.getElementById("backgroundColor");
-	var bgColor = selectObj.value;
+	var bgColor = bgc = selectObj.value;
 
 	context.fillStyle = bgColor;
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
 }
-
-// Draws a square at a random location
-function drawSquare(canvas, context) {
-	var w = Math.floor(Math.random() * 40);    
-	var x = Math.floor(Math.random() * canvas.width);
-	var y = Math.floor(Math.random() * canvas.height);
-
-	// Use this fillStyle instead if you want to try
-	// "twitter blue"
-	//context.fillStyle = "rgb(0, 173, 239)";
-	context.fillStyle = itemColor.value;
-	context.fillRect(x, y, w, w);
+// draw items at a random location
+function drawShape(canvas, context, shape){
+	if (shape == "squares") {
+		for (var i = 0; i < 20; i++) {
+			context.rect(sx[i], sy[i], sw[i], sw[i]);
+			context.fillStyle = ic = itemColor.value;
+			context.fill();}
+	}
+	else if (shape == "circles") {
+		for (var i = 0; i < 20; i++) {context.beginPath();
+			context.arc(sx[i], sy[i], sw[i], 0, degreesToRadians(360), true);
+			context.fillStyle = ic = itemColor.value;
+			context.fill();}
+	}
+	else{
+		itemColor.style.visibility="hidden";
+		sx=[];sy=[];sw=[];
+	}
 }
 
-// Draws a circle at a random location
-function drawCircle(canvas, context) {
-	var radius = Math.floor(Math.random() * 40);
-	var x = Math.floor(Math.random() * canvas.width);
-	var y = Math.floor(Math.random() * canvas.height);
-
-	context.beginPath();
-	context.arc(x, y, radius, 0, degreesToRadians(360), true);
-
-	// Use this fillStyle instead if you want to try
-	// "twitter blue"
-	//context.fillStyle = "rgb(0, 173, 239)";
-	context.fillStyle = itemColor.value;
-	context.fill();
-}
 
 // draws all the text, including the tweet
 function drawText(canvas, context) {
 	var selectObj = document.getElementById("foregroundColor");
 	
-	var fgColor = selectObj.value;
+	var fgColor = fgc = selectObj.value;
 
 	context.fillStyle = fgColor;
 	context.font = "bold 1em sans-serif";
@@ -90,10 +110,16 @@ function drawText(canvas, context) {
 
 	// draw the tweet!
 	selectObj = document.getElementById("tweets");
-	var tweet = selectObj.value;
-	context.font = "italic "+fontSize.value+"em serif";
+	var tweet = tw = selectObj.value;
+	context.save();
+	context.translate(canvas.clientWidth / 2 ,canvas.clientHeight / 2);
+	dr=draai.value;
+	context.rotate(dr * Math.PI / 180);
+	fs=fontSize.value;
+	context.font = "italic "+fs+"em serif";
 	context.textAlign = "center";
-	context.fillText(tweet, canvas.clientWidth / 2 , 100);
+	context.fillText(tweet, 0,0);
+	context.restore();
 
 	// If you want to try splitIntoLines to 
 	// handle longer tweets, uncomment this code
