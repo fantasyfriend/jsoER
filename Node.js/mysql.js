2348e10e8eab6938f17cbe851bad40ec2124dfe9
fantasyfriend <erwinvanhouten@yahoo.com>
2018-02-25 22:58:22 +0100

    database

--- Node.js/mysql.js
+++ Node.js/mysql.js
@@ -5,14 +5,14 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'root',
-  database : 'deschop',
-  port     : 3306
+  password : '',//root
+  database : 'test',//deschop
+  //port     : 3306
 });
 
 connection.connect();
 
-connection.query('SELECT * from planten', function(err, rows, fields) {
+connection.query('SELECT * from friend', function(err, rows, fields) {
   if (!err){
     var result = JSON.stringify(rows);
     console.log(result);
